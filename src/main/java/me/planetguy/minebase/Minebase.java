e7f986f791ebe9f589021b6f1addc1beb696f8ab
Planetguy <apricefrench2d@gmail.com>
2014-05-14 20:47:47 -0700

    Auto-commit (every 15 mins

--- src/main/java/me/planetguy/minebase/Minebase.java
+++ src/main/java/me/planetguy/minebase/Minebase.java
@@ -2,13 +2,14 @@ package me.planetguy.minebase;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "planetguy_Minebase")
 public class Minebase {
 	
 	@EventHandler
-	public void doPreInit(){
-		
+	public void doPreInit(FMLPreInitializationEvent ev){
+		//Test change
 	}
 
 }
