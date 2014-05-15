84e567fdcb0609547469d4530399f028ed5d4fb5
Planetguy <apricefrench2d@gmail.com>
2014-05-14 20:41:21 -0700

    Auto-commit (every 15 mins

---
+++ src/main/java/me/planetguy/minebase/Minebase.java
@@ -0,0 +1,14 @@
+package me.planetguy.minebase;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+
+@Mod(modid = "planetguy_Minebase")
+public class Minebase {
+	
+	@EventHandler
+	public void doPreInit(){
+		
+	}
+
+}
