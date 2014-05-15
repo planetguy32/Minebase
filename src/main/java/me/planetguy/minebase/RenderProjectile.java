71b2fa3057cb948a31a4dee1df88ea10f1cd6c40
Planetguy <apricefrench2d@gmail.com>
2014-05-15 15:50:01 -0700

    Auto-commit (every 10 mins)

--- src/main/java/me/planetguy/minebase/RenderProjectile.java
+++ src/main/java/me/planetguy/minebase/RenderProjectile.java
@@ -1,5 +1,33 @@
 package me.planetguy.minebase;
 
-public class RenderProjectile {
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
+
+public class RenderProjectile extends Render{
+
+	ResourceLocation texture=new ResourceLocation("textures/projectile.png");
+	
+	IModelCustom model=AdvancedModelLoader.loadModel(new ResourceLocation("/projectile.obj"));
+	
+	@Override
+	public void doRender(Entity var1, double x, double y, double z,	float whatAre, float theseFor) {
+		EntityProjectile entity=(EntityProjectile)var1;
+		GL11.glPushMatrix();
+		GL11.glTranslated(x, y, z);
+		Minecraft.getMinecraft().renderEngine.bindTexture(texture);
+		model.renderAll();
+		GL11.glPopMatrix();
+	}
+
+	@Override
+	protected ResourceLocation getEntityTexture(Entity var1) {
+		return null;
+	}
 
 }
