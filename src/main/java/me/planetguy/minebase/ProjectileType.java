8aa74108072e29ee6e6503d41931de176e7a0bdb
Planetguy <apricefrench2d@gmail.com>
2014-05-15 12:30:01 -0700

    Auto-commit (every 10 mins)

--- src/main/java/me/planetguy/minebase/ProjectileType.java
+++ src/main/java/me/planetguy/minebase/ProjectileType.java
@@ -2,10 +2,31 @@ package me.planetguy.minebase;
 
 public enum ProjectileType {
 	
-	;
+	TOWER(1),
+	BOMB(1),
+	ANTI_AIR(1),
+	BRIDGE(1), 
+	CLUSTER(1),
+	REPAIR(1),
 	
-	private ProjectileType(){
-		
+	MISSILE(3),
+	EMP(3),
+	SPIKE(3),
+	RECLAIM(3),
+	BALLOON(3),
+	MINE(3),
+	
+	CRAWLER(7),
+	VIRUS(7),
+	ENERGY(7),
+	SHIELD(7),
+	OFFENSE(7),
+	HUB(7);
+	
+	public final int cost;
+	
+	private ProjectileType(int cost){
+		this.cost=cost;
 	}
 
 }
