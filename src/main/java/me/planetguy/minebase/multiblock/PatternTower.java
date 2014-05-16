3dced42f31299c7dc0e1b86ea22f1a322eb70b71
Planetguy <apricefrench2d@gmail.com>
2014-05-16 16:20:03 -0700

    Auto-commit (every 10 mins)

--- src/main/java/me/planetguy/minebase/multiblock/PatternTower.java
+++ src/main/java/me/planetguy/minebase/multiblock/PatternTower.java
@@ -1,5 +1,13 @@
 package me.planetguy.minebase.multiblock;
 
+import me.planetguy.minebase.Minebase;
+import net.minecraft.world.World;
+
 public class PatternTower {
+	
+	public static void build(World w, int x, int y, int z, String parent) {
+		w.setBlock(x, y, z, Minebase.instance.mainBlock, 3, 3);
+		
+	}
 
 }
