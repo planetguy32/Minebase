13656a3e2aa5439e132229f6e616708a357fb8b1
Planetguy <apricefrench2d@gmail.com>
2014-05-15 20:00:01 -0700

    Auto-commit (every 10 mins)

--- src/main/java/me/planetguy/minebase/multiblock/PatternHub.java
+++ src/main/java/me/planetguy/minebase/multiblock/PatternHub.java
@@ -5,11 +5,20 @@ import net.minecraft.world.World;
 
 public class PatternHub implements IPattern{
 
+	public int platformSize=2;
+	
 	@Override
 	public void build(World w, int x, int y, int z) {
-		w.setBlock(x,y,z, Minebase.instance.mainBlock);
 		w.setBlock(x,y+1,z, Minebase.instance.mainBlock);
 		w.setBlock(x,y+2,z, Minebase.instance.mainBlock);
+		w.setBlockMetadataWithNotify(x, y+1, z, 1, 3);
+		
+		for(int dx=-platformSize; dx<=platformSize; dx++){
+			for(int dz=-platformSize; dz<=platformSize; dz++){
+				w.setBlock(x+dx, y, z+dz, Minebase.instance.mainBlock);
+				w.setBlockMetadataWithNotify(x+dx, y, z+dz, 2, 3);
+			}
+		}
 	}
 	
 
